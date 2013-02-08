954e659b6bfb562e63cc003d23e9dc7375581e99
wongingho <wonderino@gmail.com>
2013-02-08 22:02:35 +0900

    add a timer function for animations

--- template.js
+++ template.js
@@ -1,6 +1,8 @@
 var margin = {top: 40, right: 40, bottom: 40, left: 40},
     width = 960,
-    height = 500;
+    height = 500
+    start = Date.now(),
+    speed = 0.25;
     
 var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -8,4 +10,27 @@ var svg = d3.select("body").append("svg")
     .attr("class", "dot chart")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+var data = [{x:width/2, y:height/2}];
+
+var widthW = width/10;
+svg.selectAll(".rects")
+    .data(data)
+  .enter().append("rect")
+    .attr("class", "rect")
+    .attr("x", -widthW/2)
+    .attr("y", -widthW/2)
+    .attr("width", widthW)
+    .attr("height", widthW)
+    .attr("transform", function(d) { 
+    	return "translate("+ d.x + "," + d.y + ")"})
+    .style("fill", "steelblue");
+
+d3.timer(function() {
+  var deg = (Date.now() - start) * speed ; 
+  var transVal = function(d) { return "translate("+ d.x + "," + d.y + ")" + "rotate(" + deg + ")"; }
+  svg.selectAll(".rect").attr("transform", transVal);
+  //svg.attr("transform", transVal); // fixed ring*/
+}); 
 
