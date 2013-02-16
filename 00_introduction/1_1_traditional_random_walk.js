c1498c8dd0e0038d22ab3af13e6120f9b52d0e32
wongingho <wonderino@gmail.com>
2013-02-16 21:10:31 +0900

    1.1 traditional_random_walk

--- 00_introduction/1_1_traditional_random_walk.js
+++ 00_introduction/1_1_traditional_random_walk.js
@@ -1,16 +1,16 @@
 var margin = {top: 40, right: 40, bottom: 40, left: 40},
-    width = 600,
-    height = 400
+    width = 640,
+    height = 240,
     start = Date.now(),
     speed = 0.25;
     
-var svg = d3.select("body")
+var svg = d3.select("#trad_rand_walk")
   .append("svg")
     .attr("width", width)
     .attr("height", height)
     .attr("class", "canvases")
   .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    //.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 var data = [{x:width/2, y:height/2}];
@@ -46,7 +46,7 @@ function Walker() {
 }
 
 var walker = new Walker();
-
+walker.display();
 
 d3.timer(function() {
   var deg = (Date.now() - start) * speed ; 
