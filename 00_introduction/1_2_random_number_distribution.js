f27472871ee479d73e4c5f1f7f910bf992e6b795
wongingho <wonderino@gmail.com>
2013-02-16 21:10:46 +0900

    1.2 Random Number Distribution

---
+++ 00_introduction/1_2_random_number_distribution.js
@@ -0,0 +1,40 @@
+var margin = {top: 40, right: 40, bottom: 40, left: 40},
+    width = 640,
+    height = 240,
+    start = Date.now(),
+    speed = 0.25;
+    
+var svg = d3.select("#rand_num_dist")
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "canvases")
+  .append("g")
+  
+
+var randomCounts = [];
+for (var i = 0; i < 20; i++) {
+  randomCounts.push(0);
+}
+var w = width/randomCounts.length;
+
+var rects = svg.selectAll(".rect")
+    .data(randomCounts)
+  .enter().append("rect")
+    .attr("class", "rect")
+    .attr("x", function(d,i) {return w * i -.5;})
+    .attr("y", function(d) {return height - d;})
+    .attr("width", w)
+    .attr("height", function(d) {return d})
+    .attr("fill", "steelblue");
+
+d3.timer(function() {
+  var index = Math.floor(Math.random() * randomCounts.length);
+  randomCounts[index] += 1;
+  console.log(index);
+
+  rects.data(randomCounts)
+    .attr("y", function(d) {return height - d})
+    .attr("height", function(d) {return d});
+}); 
+
